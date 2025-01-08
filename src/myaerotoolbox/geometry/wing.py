32fdb59a9fb1908c30dc4a159a2d3bdeb52fd479
tleniger <95964164+TiesLeniger@users.noreply.github.com>
2025-01-08 19:35:37 +0100

    added a bit

--- src/myaerotoolbox/geometry/wing.py
+++ src/myaerotoolbox/geometry/wing.py
@@ -1,83 +1,65 @@
 import numpy as np
-from typing import Union
 from ..helpers import rotate_points_about_x_axis, rotate_points_about_y_axis, rotate_points_about_z_axis, rotate_points_about_arbitrary_axis
 import logging
 
 logger = logging.getLogger(__name__)
 class WingSection:
 
-    def __init__(self, num_chordwise: int, num_spanwise: int, airfoils: Union[list, tuple],
+    def __init__(self, number: int, num_chordwise: int, num_spanwise: int, airfoils: list[object],
                  rootLE: np.ndarray, rootchord: float, taper_ratio: float, span: float, 
                  sweep_angle: float, sweep_loc: float, twist_angle: float, twist_loc: float, 
-                 dihedral: float, angle_units: str = "deg"
+                 dihedral: float, chordwise_spacing: str = "cosine", angle_units: str = "deg", 
+                 parent_section: object = None
                  ):
-        if num_chordwise < 1:
-            logger.error(f"Specify at least 1 chordwise panel")
-            raise ValueError(f"Specify at least 1 chordwise panel")
-        elif num_spanwise < 1:
-            logger.error(f"Specify at least 1 spanwise panel")
-            raise ValueError(f"Specify at least 1 spanwise panel")
-        if rootLE.ndim != 1 or rootLE.shape != 3:
-            logger.error(f"rootLE must be given as a 1D numpy array of shape (3,), got {rootLE.shape}")
-            raise ValueError(f"rootLE must be given as a 1D numpy array of shape (3,), got {rootLE.shape}")
-        else:
-            self.rootLE = rootLE                                # Leading edge point of the root of the section (geometry is built up w.r.t this point)
-        if rootchord < 0:
-            logger.error(f"The root chord can not be negative, got {rootchord}")
-            raise ValueError(f"The root chord can not be negative, got {rootchord}")
-        else:
-            self.croot = rootchord                              # Root chord of the section
-        if not (0 < taper_ratio <= 1):
-            logger.error(f"The value for the taper ratio should be 0 < taper <= 1, got {taper_ratio}")
-            raise ValueError(f"The value for the taper ratio should be 0 < taper <= 1, got {taper_ratio}")
-        else:
-            self.taper = taper_ratio                            # Taper ratio of the section (tip chord/root chord)
-        self.ctip = rootchord * taper_ratio                     # Tip chord of the section
-        if span <= 0:
-            logger.error(f"The span can not be negative or 0, got {span}")
-            raise ValueError(f"The span can not be negative or 0, got {span}")
-        else:
-            self.span = span                                    # Section span 
+        self.nc = num_chordwise                             # Number of chordwise panels used to discretise the section
+        self.chordspace = chordwise_spacing                 # Chordwise spacing method (constant or cosine)
+        self.ns = num_spanwise                              # Number of spanwise panels used to discretise the section
+        self.airfoils = airfoils                            # List or tuple of airfoil digits (strings)
+        self.rootLE = rootLE                                # [x, y, z] location of the leadig edge in meter
+        self.croot = rootchord                              # Root chord in meter
+        self.taper = taper_ratio                            # Taper ratio (tip chord / root chord) [-]
+        self.ctip = rootchord * taper_ratio                 # Tip chord in meter
+        self.span = span                                    # Section span
         if angle_units == "deg":
-            self.sweep = np.deg2rad(sweep_angle)                # Sweep angle of the section
-            self.twist = np.deg2rad(twist_angle)                # Twist angle of the section
-            self.dihedral = np.deg2rad(dihedral)                # Dihedral angle of the section
+            self.sweep = np.deg2rad(sweep_angle)            # Sweep angle converted to rad        
+            self.twist = np.deg2rad(twist_angle)            # Twist angle converted to rad
+            self.dihedral = np.deg2rad(dihedral)            # Dihedral angle converted to rad
         elif angle_units == "rad":
             self.sweep = sweep_angle
             self.twist = twist_angle
-            self.dihedral = dihedral
-        else:
-            logger.error(f"angle_units got an unexpected argument ({angle_units}). Must be 'deg' or 'rad'")
-            raise ValueError(f"angle_units got an unexpected argument ({angle_units}). Must be 'deg' or 'rad'")
-        if not 0 <= sweep_loc <= 1:
-            logger.error(f"The sweep location should be between 0 and 1 (fraction of the chord)")
-            raise ValueError(f"The sweep location should be between 0 and 1 (fraction of the chord)")
-        elif not 0 <= twist_loc <= 1:
-            logger.error(f"The twist location should be between 0 and 1 (fraction of the chord)")
-            raise ValueError(f"The twist location should be between 0 and 1 (fraction of the chord)")
-        else:
-            self.twistloc = twist_loc
-            self.sweeploc = sweep_loc
-        self.rootaf = airfoils[0]
-        self.tipaf = airfoils[1]
+            self.dihedral = dihedral                    
+        self.sweeploc = sweep_loc                           # Chordwise location at which the sweep angle is specified (0 is LE, 1 is TE)
+        self.twistloc = twist_loc                           # Chordwise location at which the twist is applied (0 is LE, 1 is TE)
+        self.controlsurfaces = {}                           # Initialise a control surface dict
+        if parent_section is not None:                      # Parent section for alignment
+            if number == 1:
+                logger.warning("Sections with number 1 do not need a parent section")
+            else:
+                self.parent = parent_section
+        elif parent_section is None and number > 1:
+            logger.error("Sections with a number higher than 1 must have a parent section for alignment")
+            raise ValueError("Sections with a number higher than 1 must have a parent section for alignment")
 
-    def apply_twist(self, tip: np.ndarray):
-        """
-        Function that twists the tip with `self.twist` before interpolation between root and tip
+    def add_controlsurface(self, ID: str, y_bstart: float, y_bstop: float, x_cstart: float, max_up: float, max_down: float):
+        pass
 
-        Parameters:
-        tip : np.ndarray
-            (points_chord, 3) shaped array (3 for x, y and z) that captures the camberline of the tip of the section
-        """
-        LEx = tip[0, 0]
-        tip[:, 0] = tip[:, 0] - LEx - self.twistloc * self.ctip           # Translate the tip so that the twistloc is on the y axis
-        tip = rotate_points_about_y_axis(tip, self.twist, unit = "rad")         # Apply the rotation
-        tip[:, 0] = tip[:, 0] + LEx + self.twistloc * self.ctip
-
-        return tip
-    
-
+    def __align_with_parent(self, num_tol: float = 1e-9):
+        if abs(self.parent.ctip - self.croot) > num_tol:
+            logger.warning("Parent tip chord is different from root chord. Changing root chord to parent tip chord for alignment")
+            self.croot = self.parent.ctip
+        if abs(self.parent.twist) > num_tol:
+            if not hasattr(self.airfoils[0], 'camberline'):
+                self.airfoils[0].calculate_camberline(self.nc, self.chordspace)
+            self.airfoils[0].camberline[:, 0] -= self.parent.twist_loc * self.parent.ctip
+            self.airfoils[0].camberline = rotate_points_about_y_axis(self.airfoils[0].camberline, self.parent.twist, unit = "rad")
+            self.airfoils[0].camberline[:, 0] += self.parent.twist_loc * self.parent.ctip
+        if not hasattr(self.parent, 'sectionpoints'):
+            self.parent.generate_section_geometry()
+        self.rootLE = self.parent.sectionpoints[0, -1, :]
+        self.airfoils[0].camberline[0, :] = self.rootLE
 
 
+    def generate_section_geometry(self):
+        pass
 
-
+                  
